0623253bfd10f4bb843ad23c3d1af1ded519c313
romaindupont <rd.romaindupont@free.fr>
2021-08-21 16:12:05 +0200

    3D en cours

--- src/index.js
+++ src/index.js
@@ -96,6 +96,8 @@ const car = () => {
   const scene = new THREE.Scene();
   scene.background = new THREE.Color( 0xeeeeee );
   scene.environment = pmremGenerator.fromScene( new RoomEnvironment() ).texture;
+  /* const light = new THREE.AmbientLight( 0x000000, 0.5);
+  scene.add( light ); */
   /* scene.fog = new THREE.Fog( 0xeeeeee, 10, 100 );  */
   /* scene.add(new THREE.AmbientLight( 0x666666 )) */
  /*  const light = new THREE.DirectionalLight( 0xdfebff, 1 );
@@ -122,32 +124,46 @@ const car = () => {
   grid.material.depthWrite = true;
   grid.material.transparent = true;
   scene.add( grid );
-    const bodyMaterial = new THREE.MeshPhysicalMaterial( {
-      color: 0x14571D, metalness: 0.6, roughness: 0.4, clearcoat: 0.5, clearcoatRoughness: 0.5
-    } );
-    const detailsMaterial = new THREE.MeshStandardMaterial( {
-      color: 0x201616, metalness: 1.0, roughness: 0.5
-    } );
-
-    const glassMaterial = new THREE.MeshPhysicalMaterial( {
-      color: 0xffffff, metalness: 0, roughness: 0, transmission: 0.9, transparent: true
-    } );
-
-    const seatMaterial = new THREE.MeshPhysicalMaterial( {
-      color: 0xa97843, metalness: 0.01, roughness: 0.4, clearcoat: 0.5, clearcoatRoughness: 0.5
+  const texture = new THREE.TextureLoader().load('../src/assets/d/leather.png', function ( map ) {
+    map.wrapS = THREE.RepeatWrapping;
+    map.wrapT = THREE.RepeatWrapping;
+    map.anisotropy = 10;
+    map.repeat.set( 34, 34 );
+  });
+  const bodyMaterial = new THREE.MeshPhysicalMaterial( {
+    color: 0x0d1c08, metalness: 0.6, roughness: 1, clearcoat: 0.6, clearcoatRoughness: 0.2
+  } );
+  const detailsMaterial = new THREE.MeshStandardMaterial( {
+    color: 0x4C4C53, metalness: 1.0, roughness: 0.5, emissive: 0x000000
+  } );
+  
+  const glassMaterial = new THREE.MeshPhysicalMaterial( {
+    color: 0xffffff, metalness: 0, roughness: 0, transmission: 0.9, transparent: true
+  } );
+  
+  /* const seatMaterial = new THREE.MeshBasicMaterial( { map: texture, color: 0xa97843 } ) */
+  /* const seatMaterial = new THREE.MeshPhysicalMaterial( {
+    color: 0xa97843, metalness: 0.01, roughness: 0.4, clearcoat: 0.5, clearcoatRoughness: 0.5
     } );/* C29565 */
-
+    const seatMaterial = new THREE.MeshStandardMaterial( {
+      color: 0xC2531E, metalness: 0, roughness: 0.9, emissive: 0x000000
+    } );
     const bodyColorInput = document.getElementById( 'body-color' );
-    bodyColorInput.addEventListener( 'input', function () {
+    bodyColorInput.addEventListener( 'click', function () {
 
-      bodyMaterial.color.set( this.value );
-
+      bodyMaterial.color.set('#541919' );/* B84D4D FF0000*/
+      detailsMaterial.color.set( '#535353' );
+      seatMaterial.color.set( '#C2531E' );/* C29565 */
+      /* seatMaterial = new THREE.MeshBasicMaterial( { map: texture, color: 0xa97843 } ) */
+      /* seatMaterial.map.set(texture) */
     } );
 
     const detailsColorInput = document.getElementById( 'details-color' );
-    detailsColorInput.addEventListener( 'input', function () {
+    detailsColorInput.addEventListener( 'click', function () {
 
-      detailsMaterial.color.set( this.value );
+      bodyMaterial.color.set('#0d1c08' );
+      detailsMaterial.color.set( '#4C4C53' );
+      seatMaterial.color.set( '#C2531E' );
 
     } );
 
@@ -167,21 +183,22 @@ const car = () => {
   loader.setDRACOLoader( dracoLoader );
   loader.load('../src/assets/d/124_spider_1970.glb', function (gltf) {
     const carModel = gltf.scene.children[2];
-    console.log(carModel)
-     carModel.getObjectByName( 'Fiat70_Spider_body' ).material = bodyMaterial;
+    carModel.getObjectByName( 'Fiat70_Spider_body' ).material = bodyMaterial;
      /* carModel.getObjectByName( 'Fiat70_Spider_body1' ).material = bodyMaterial; */
-      carModel.getObjectByName( 'Fiat70_Spider_body2' ).material = bodyMaterial;
-     carModel.getObjectByName( 'Fiat70_Spider_body3' ).material = bodyMaterial;
-     carModel.getObjectByName( 'Fiat70_Spider_body4' ).material = bodyMaterial;
-     carModel.getObjectByName( 'Fiat70_Spider_body5' ).material = bodyMaterial;
+    carModel.getObjectByName( 'Fiat70_Spider_body2' ).material = bodyMaterial;
+    carModel.getObjectByName( 'Fiat70_Spider_body3' ).material = bodyMaterial;
+    carModel.getObjectByName( 'Fiat70_Spider_body4' ).material = bodyMaterial;
+    carModel.getObjectByName( 'Fiat70_Spider_body5' ).material = bodyMaterial;
      /* carModel.getObjectByName( 'Fiat70_Spider_body6' ).material = bodyMaterial; */
     
-     carModel.getObjectByName( 'Fiat70_Spider_body7' ).material = bodyMaterial;
-     carModel.getObjectByName( 'Fiat70_Spider_pettycap' ).material = bodyMaterial;
+    carModel.getObjectByName( 'Fiat70_Spider_body7' ).material = bodyMaterial;
+    carModel.getObjectByName( 'Fiat70_Spider_pettycap' ).material = bodyMaterial;
 
-     carModel.getObjectByName( 'Fiat70_Spider_roof' ).material = detailsMaterial;
+    carModel.getObjectByName( 'Fiat70_Spider_roof' ).material = detailsMaterial;
     carModel.getObjectByName( 'Fiat70_Spider_seats1' ).material = seatMaterial;
     carModel.getObjectByName( 'Fiat70_Spider_seats2' ).material = seatMaterial;
+    /* carModel.getObjectByName( 'Fiat70_Spider_interior1' ).material = seatMaterial; */
+    carModel.getObjectByName( 'Fiat70_Spider_interior2' ).material = seatMaterial;
     /*carModel.getObjectByName( 'Fiat70_Spider_tyre4' ).material = detailsMaterial; */
 /*     carModel.getObjectByName( 'rim_fr' ).material = detailsMaterial;
     carModel.getObjectByName( 'rim_rr' ).material = detailsMaterial;
