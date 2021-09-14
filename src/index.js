c590174070a43fbede6a602db3746b12eb6a4da7
romaindupont <rd.romaindupont@free.fr>
2021-09-14 15:48:53 +0200

    emailjs + message ok

--- src/index.js
+++ src/index.js
@@ -70,63 +70,46 @@ const sendMail = () => {
   const sending = document.querySelector('.form');
   sending.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(e.target.nom.value)/* prenom email telephone text */
-  })
+    emailjs.sendForm('service_3slf6uv', 'template_129bneo', e.target, 'user_w101tU28ZhbwEv5HUCLDL')
+      .then((result) => {
+          const messageSend = document.getElementById('reponse');
+          messageSend.style.display='block';
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+     
+  }) 
 }
 
 const car = () => {
-  const wheels = [];
   const container = document.getElementById( 'container' );
   const renderer = new THREE.WebGLRenderer( { antialias: true } );
   renderer.setPixelRatio( window.devicePixelRatio );
   renderer.setSize( window.innerWidth, window.innerHeight );
   renderer.setAnimationLoop( render );
+
   renderer.outputEncoding = THREE.RGBDEncoding;
   renderer.toneMapping = THREE.ACESFilmicToneMapping;
   renderer.toneMappingExposure = 0.85;
   container.appendChild( renderer.domElement );
 
-  /* window.addEventListener( 'resize', onWindowResize ); */
-  const camera = new THREE.PerspectiveCamera( 20, window.innerWidth / window.innerHeight, 0.1, 1000 );
-  /* camera.position.set( -3.25, 8.4, 5.5 ); */
-  
-  camera.position.set( -15, 5, 10 );/* camera.zoom=6.8 */
- /*  camera.maxDistance.set(1) */
+  const camera = new THREE.PerspectiveCamera( 5, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  camera.position.set( 30, 5, 20 );
+
   const controls = new OrbitControls( camera, container );
+  /* controls.addEventListener( 'change', animate ); */
   controls.target.set( 0, 0.5, 0 );
   controls.minDistance = 0.8;
   controls.minZoom = 0.5;
-  /* camera.position.set( 100, 60, 50 ); */
-  controls.update();
+  /* controls.update(); */
 
   const pmremGenerator = new THREE.PMREMGenerator( renderer );
 
   const scene = new THREE.Scene();
   scene.background = new THREE.Color( 0xeeeeee );
   scene.environment = pmremGenerator.fromScene( new RoomEnvironment() ).texture;
-  /* const light = new THREE.AmbientLight( 0x000000, 0.5);
-  scene.add( light ); */
-  /* scene.fog = new THREE.Fog( 0xeeeeee, 10, 100 );  */
-  /* scene.add(new THREE.AmbientLight( 0x666666 )) */
- /*  const light = new THREE.DirectionalLight( 0xdfebff, 1 );
-				light.position.set( 50, 200, 100 );
-				light.position.multiplyScalar( 1.3 );
-
-				light.castShadow = true;
-
-				light.shadow.mapSize.width = 1024;
-				light.shadow.mapSize.height = 1024;
-
-				const d = 300;
-
-				light.shadow.camera.left = - d;
-				light.shadow.camera.right = d;
-				light.shadow.camera.top = d;
-				light.shadow.camera.bottom = - d;
-
-				light.shadow.camera.far = 1000;
-
-				scene.add( light ); */
+ 
   const grid = new THREE.GridHelper( 10, 0, 0xeeeeee, 0xeeeeee );
   grid.material.opacity = 0.1;
   grid.material.depthWrite = true;
@@ -139,132 +122,119 @@ const car = () => {
     map.repeat.set( 34, 34 );
   });
   const bodyMaterial = new THREE.MeshPhysicalMaterial( {
-    color: 0x0d1c08, metalness: 0.6, roughness: 1, clearcoat: 0.6, clearcoatRoughness: 0.2
+    color: 0xd31426, metalness: 0.6, roughness: 1, clearcoat: 0.6, clearcoatRoughness: 0.2
   } );
   const detailsMaterial = new THREE.MeshStandardMaterial( {
     color: 0x4C4C53, metalness: 1.0, roughness: 0.5, emissive: 0x000000
   } );
-  
   const glassMaterial = new THREE.MeshPhysicalMaterial( {
     color: 0xffffff, metalness: 0, roughness: 0, transmission: 0.9, transparent: true
   } );
-  
-  /* const seatMaterial = new THREE.MeshBasicMaterial( { map: texture, color: 0xa97843 } ) */
-  /* const seatMaterial = new THREE.MeshPhysicalMaterial( {
-    color: 0xa97843, metalness: 0.01, roughness: 0.4, clearcoat: 0.5, clearcoatRoughness: 0.5
-    } );/* C29565 */
-    const seatMaterial = new THREE.MeshStandardMaterial( {
-      color: 0xC2531E, metalness: 0, roughness: 0.9, emissive: 0x000000
-    } );
-    const bodyColorInput = document.getElementById( 'body-color' );
-    bodyColorInput.addEventListener( 'click', function () {
+  const seatMaterial = new THREE.MeshStandardMaterial( {
+    color: 0xC2531E, metalness: 0, roughness: 0.9, emissive: 0x000000
+  } );
 
-      bodyMaterial.color.set('#541919' );/* B84D4D FF0000*/
-      detailsMaterial.color.set( '#535353' );
-      seatMaterial.color.set( '#C2531E' );/* C29565 */
-      /* seatMaterial = new THREE.MeshBasicMaterial( { map: texture, color: 0xa97843 } ) */
-      /* seatMaterial.map.set(texture) */
-    } );
+  const parisCarInput = document.getElementById( 'parisCar' );
+  parisCarInput.addEventListener( 'click', function () {
 
-    const detailsColorInput = document.getElementById( 'details-color' );
-    detailsColorInput.addEventListener( 'click', function () {
+    bodyMaterial.color.set('#d31426' );
+    detailsMaterial.color.set( '#535353' );
+    seatMaterial.color.set( '#141313' );
+  } );
 
-      bodyMaterial.color.set('#0d1c08' );
-      detailsMaterial.color.set( '#4C4C53' );
-      seatMaterial.color.set( '#C2531E' );
+  const dublinCarInput = document.getElementById( 'dublinCar' );
+  dublinCarInput.addEventListener( 'click', function () {
 
-    } );
+    bodyMaterial.color.set('#0d1c08' );
+    detailsMaterial.color.set( '#0d1c08' );
+    seatMaterial.color.set( '#C2531E' );
+  } );
 
-    const glassColorInput = document.getElementById( 'glass-color' );
-    glassColorInput.addEventListener( 'click', function () {
+  const osloCarInput = document.getElementById( 'osloCar' );
+  osloCarInput.addEventListener( 'click', function () {
 
-      bodyMaterial.color.set('#021250' );
-      detailsMaterial.color.set( '#4C4C53' );
-      seatMaterial.color.set( '#C2531E' );
+    bodyMaterial.color.set('#021250' );
+    detailsMaterial.color.set( '#021250' );
+    seatMaterial.color.set( '#C2531E' );
+  } );
 
-    } );
-    const milanColorInput = document.getElementById( 'milanCar' );
-    milanColorInput.addEventListener( 'click', function () {
+  const casablancaCarInput = document.getElementById( 'casablancaCar' );
+  casablancaCarInput.addEventListener( 'click', function () {
 
-      bodyMaterial.color.set('#fff' );
-      detailsMaterial.color.set( '#4C4C53' );
-      seatMaterial.color.set( '#C2531E' );
+    bodyMaterial.color.set('#fff' );
+    detailsMaterial.color.set( '#4C4C53' );
+    seatMaterial.color.set( '#C2531E' );
+  } );
 
-    } );
+  const milanCarInput = document.getElementById( 'milanCar' );
+  milanCarInput.addEventListener( 'click', function () {
+
+    bodyMaterial.color.set('#d31426' );
+    detailsMaterial.color.set( '#535353' );
+    seatMaterial.color.set( '#C2531E' );
+  } );
+
+  const moscouCarInput = document.getElementById( 'moscouCar' );
+  moscouCarInput.addEventListener( 'click', function () {
+
+    bodyMaterial.color.set('#fff' );
+    detailsMaterial.color.set( '#fff' );
+    seatMaterial.color.set( '#d31426' );
+  } );
+
+  const lisbonneCarInput = document.getElementById( 'lisbonneCar' );
+  lisbonneCarInput.addEventListener( 'click', function () {
+
+    bodyMaterial.color.set('#b6ab00' );
+    detailsMaterial.color.set( '#b6ab00' );
+    seatMaterial.color.set( '#C2531E' );
+  } );
+
   const loader = new GLTFLoader();
   const dracoLoader = new DRACOLoader();
-  /* dracoLoader.setDecoderPath( 'src/three/examples/js/libs/draco/' ); */
   dracoLoader.setDecoderPath('../src/assets/gltf/');
-  /* const dracoLoader = new DRACOLoader() */
-  /* loader.setDRACOLoader( dracoLoader ); */
   loader.setDRACOLoader( dracoLoader );
   loader.load('../src/assets/d/124_spider_1970.glb', function (gltf) {
     const carModel = gltf.scene.children[2];
     carModel.getObjectByName( 'Fiat70_Spider_body' ).material = bodyMaterial;
-     /* carModel.getObjectByName( 'Fiat70_Spider_body1' ).material = bodyMaterial; */
     carModel.getObjectByName( 'Fiat70_Spider_body2' ).material = bodyMaterial;
     carModel.getObjectByName( 'Fiat70_Spider_body3' ).material = bodyMaterial;
     carModel.getObjectByName( 'Fiat70_Spider_body4' ).material = bodyMaterial;
     carModel.getObjectByName( 'Fiat70_Spider_body5' ).material = bodyMaterial;
-     /* carModel.getObjectByName( 'Fiat70_Spider_body6' ).material = bodyMaterial; */
-    
     carModel.getObjectByName( 'Fiat70_Spider_body7' ).material = bodyMaterial;
     carModel.getObjectByName( 'Fiat70_Spider_pettycap' ).material = bodyMaterial;
 
     carModel.getObjectByName( 'Fiat70_Spider_roof' ).material = detailsMaterial;
+
     carModel.getObjectByName( 'Fiat70_Spider_seats1' ).material = seatMaterial;
     carModel.getObjectByName( 'Fiat70_Spider_seats2' ).material = seatMaterial;
-    /* carModel.getObjectByName( 'Fiat70_Spider_interior1' ).material = seatMaterial; */
     carModel.getObjectByName( 'Fiat70_Spider_interior2' ).material = seatMaterial;
-    /*carModel.getObjectByName( 'Fiat70_Spider_tyre4' ).material = detailsMaterial; */
-/*     carModel.getObjectByName( 'rim_fr' ).material = detailsMaterial;
-    carModel.getObjectByName( 'rim_rr' ).material = detailsMaterial;
 
-    carModel.getObjectByName( 'rim_rl' ).material = detailsMaterial;
-    carModel.getObjectByName( 'trim' ).material = detailsMaterial; */
+    carModel.getObjectByName( 'Fiat70_Spider_glass' ).material = glassMaterial;
 
-     carModel.getObjectByName( 'Fiat70_Spider_glass' ).material = glassMaterial;
-/*      wheels.push(
-      carModel.getObjectByName( 'Fiat70_Spider_protector1' ),
-      carModel.getObjectByName( 'Fiat70_Spider_protector2' ),
-      carModel.getObjectByName( 'Fiat70_Spider_protector3' ),
-      carModel.getObjectByName( 'Fiat70_Spider_protector4' )
-     ); */
-
-      scene.add( carModel );
+    scene.add( carModel );
   },
 
   function (xhr) {
-      console.log((xhr.loaded / xhr.total * 100 ) + '% loaded' );
+    console.log((xhr.loaded / xhr.total * 100 ) + '% loaded' );
   },
 
   function (error) {
-      console.log( 'An error happened = ', error );
+    console.log( 'An error happened = ', error );
   }
 );
 
-const onWindowResize = () => {
-
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-
-  renderer.setSize( window.innerWidth, window.innerHeight );
-
-}
-function render() {
-
-  const time = - performance.now() / 1000;
-
-  for ( let i = 0; i < wheels.length; i ++ ) {
-
-    wheels[ i ].rotation.x = time * Math.PI;
-
+  function render() {
+    /* controls.update();
+    requestAnimationFrame( render ); */
+    renderer.render( scene, camera );
+    
   }
-
-  grid.position.x = - ( time ) % 5;
-
-  renderer.render( scene, camera );
+/*   function animate() {
+    controls.update();
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+    
+  }   */
 }
-}
-
 app.append(time, AddLogo(), OpenCloseMenu(),CloseMenuOnClick() ,sendMail(), car())
